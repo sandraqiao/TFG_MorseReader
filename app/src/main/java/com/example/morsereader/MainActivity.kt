5c8d2a5779092fc017941d62f02b7504ba3f4b21
sandra <sandra.qiao01@gmail.com>
2026-03-21 21:41:32 +0100

    inicio de las pantallas

--- app/src/main/java/com/example/morsereader/MainActivity.kt
+++ app/src/main/java/com/example/morsereader/MainActivity.kt
@@ -3,45 +3,38 @@ package com.example.morsereader
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.morsereader.ui.navigation.Screen
+import com.example.morsereader.ui.screens.*
 import com.example.morsereader.ui.theme.MorseReaderTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MorseReaderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.Home.route
+                    ) {
+                        composable(Screen.Home.route) { HomeScreen(navController) }
+                        composable(Screen.Generate.route) { GenerateScreen() }
+                        composable(Screen.Interpret.route) { InterpretScreen() }
+                        composable(Screen.Info.route) { InfoScreen() }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MorseReaderTheme {
-        Greeting("Android")
     }
 }
