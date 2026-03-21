f7f5366151c4bc1990548ad54831c8cf61c384cd
sandra <sandra.qiao01@gmail.com>
2026-03-21 23:51:20 +0100

    Cambio de idioma de los textos, que lo queremos en español por ahoraaaa

--- app/src/main/java/com/example/morsereader/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/morsereader/ui/navigation/Screen.kt
@@ -9,9 +9,9 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Screen (val title: String, val route: String, val icon: ImageVector){
-    object Home: Screen("Home", "home", Icons.Default.Home)
-    object Generate: Screen("Generate", "generate", Icons.Default.Highlight)
-    object Interpret: Screen("Interpret", "interpret", Icons.Default.CameraAlt)
+    object Home: Screen("Inicio", "home", Icons.Default.Home)
+    object Generate: Screen("Generar", "generate", Icons.Default.Highlight)
+    object Interpret: Screen("Interpretar", "interpret", Icons.Default.CameraAlt)
     object Info: Screen("Info", "info", Icons.Default.AutoStories)
 
     companion object {
