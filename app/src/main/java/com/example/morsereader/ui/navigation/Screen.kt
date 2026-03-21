c01a287046032d0b6b774bd5a0e7db2f6bd450db
sandra <sandra.qiao01@gmail.com>
2026-03-21 23:50:07 +0100

    Cambio de icono

--- app/src/main/java/com/example/morsereader/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/morsereader/ui/navigation/Screen.kt
@@ -2,6 +2,7 @@ package com.example.morsereader.ui.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AutoStories
+import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material.icons.filled.Campaign
 import androidx.compose.material.icons.filled.Highlight
 import androidx.compose.material.icons.filled.Home
@@ -10,7 +11,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 sealed class Screen (val title: String, val route: String, val icon: ImageVector){
     object Home: Screen("Home", "home", Icons.Default.Home)
     object Generate: Screen("Generate", "generate", Icons.Default.Highlight)
-    object Interpret: Screen("Interpret", "interpret", Icons.Default.Campaign)
+    object Interpret: Screen("Interpret", "interpret", Icons.Default.CameraAlt)
     object Info: Screen("Info", "info", Icons.Default.AutoStories)
 
     companion object {
